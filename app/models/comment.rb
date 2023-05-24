7bf98a5d51bf186247569448eb35e541d0486103
Uthaeus <romanlavery@gmail.com>
2023-05-23 18:17:54 -0600

    added comment associations in model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :project, optional: true
-  belongs_to :comment, optional: true
+  belongs_to :parent, class_name: "Comment", optional: true
+
+  has_many :replies, class_name: "Comment", foreign_key: :parent_id, dependent: :destroy
 
   validates :content, presence: true
 end
