dd69a87847a6e8f80e8c91e407838801979f3053
Uthaeus <romanlavery@gmail.com>
2023-05-25 20:28:05 -0600

    rendering projects to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,15 @@ class PagesController < ApplicationController
   respond_to :json
 
   def home
-    @page_title = "Home"
-    # q: how to only include published projects in descending order?
-    # a: use the published? scope method from the Project model
-    # q:
 
-    @projects = Project.all.published?.order(created_at: :desc)
-    render json: { title: @page_title, projects: @projects}
+    @projects = Project.all
+    render json: @projects
   end
 
   def about
-    @page_title = "About"
-    render json: @page_title
   end
 
   def contact
-    @page_title = "Contact"
-    render json: @page_title
   end
 
   def user_check
@@ -27,7 +19,5 @@ class PagesController < ApplicationController
   end
 
   def options
-    @page_title = "Options"
-    render json: @page_title
   end
 end
